b721f7e77f754f8070fd33d26b169df3d05b665c
Tiago Daniel <187806573+tiagodan777@users.noreply.github.com>
2025-11-14 11:03:28 -0100

    string variable

---
+++ c02/js/string-variable.js
@@ -0,0 +1,9 @@
+var username;
+var message;
+username = 'Molly';
+message = 'Conhece a nossa próxima coleção';
+
+var elName = document.getElementById('name');
+elName.textContent = username;
+var elNote = document.getElementById('note');
+elNote.textContent = message;
