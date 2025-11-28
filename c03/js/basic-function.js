a66de287b060e8ba3378754d902a905190ae8bac
Tiago Daniel <187806573+tiagodan777@users.noreply.github.com>
2025-11-28 08:09:28 -0100

    alguns ex

---
+++ c03/js/basic-function.js
@@ -0,0 +1,6 @@
+var msg = 'Inscreve-te e recebe a nossa newsletter e ganha 10% de desconto!';
+function updateMessage() {
+    var el = document.getElementById('message');
+    el.textContent = msg;
+}
+updateMessage();
